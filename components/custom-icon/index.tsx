ccd7ee47b5ed990f3ae3939ad78e2b9e9fb5e845
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2023-11-26 14:35:18 -0300

    :sparkles: feat: create custom icon component

---
+++ components/custom-icon/index.tsx
@@ -0,0 +1,43 @@
+import { IconContext } from "react-icons";
+import { BiLogoTypescript } from "react-icons/bi";
+import { TbBrandNextjs } from "react-icons/tb";
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaInstagram,
+  FaJsSquare,
+  FaReact,
+  FaSass,
+} from "react-icons/fa";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { SiStyledcomponents, SiTailwindcss } from "react-icons/si";
+
+interface Icon {
+  icon: string;
+  color: string;
+  size: string;
+}
+
+export default function CustomIcon({ icon, color, size }: Icon) {
+  const iconMapping: any = {
+    github: FaGithub,
+    linkedin: FaLinkedinIn,
+    instagram: FaInstagram,
+    curriculum: IoDocumentTextOutline,
+    typescript: BiLogoTypescript,
+    javascript: FaJsSquare,
+    react: FaReact,
+    next: TbBrandNextjs,
+    styledcomponents: SiStyledcomponents,
+    tailwind: SiTailwindcss,
+    sass: FaSass,
+  };
+
+  const IconComponent = iconMapping[icon];
+
+  return (
+    <IconContext.Provider value={{ color, size }}>
+      <IconComponent />
+    </IconContext.Provider>
+  );
+}
