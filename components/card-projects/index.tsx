b5b110327a152c94b0144f28506a247db030fa25
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2023-12-02 11:10:31 -0300

    :sparkles: feat: created a loader function in the image

--- components/card-projects/index.tsx
+++ components/card-projects/index.tsx
@@ -1,26 +1,55 @@
+// import { client } from "@/sanity/lib/client";
+// import { useNextSanityImage } from "next-sanity-image";
+
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
 
 import Button from "@/components/button";
+import { urlFor } from "@/sanity/lib/sanity";
 
-export default function CardProjects() {
+interface CardProps {
+  srcImage?: string;
+  name: string;
+  description: string;
+  redirect: string;
+}
+
+export default function CardProjects({
+  srcImage,
+  name,
+  description,
+  redirect,
+}: CardProps) {
   return (
     <div className="mb-3 flex h-full flex-col gap-2 rounded-lg bg-b-terciary p-4 sm:w-48 md:w-56 lg:w-72 xl:w-96">
-      <Image
-        src="/rick-and-morty.png"
-        className="h-full w-full rounded-t-lg"
-        width={264}
-        height={122}
-        alt="Rick and Morty"
-      />
+      {srcImage && (
+        <Image
+          src={srcImage}
+          className="h-full w-full rounded-t-lg object-cover"
+          width={264}
+          height={122}
+          alt={name}
+          loader={({ src, width, quality }) => {
+            return urlFor(src)
+              .width(width)
+              .quality(quality || 75)
+              .url();
+          }}
+        />
+      )}
 
       <h3 className="font-heebo text-base font-semibold text-custom-primary">
-        Lorem Ipsum
+        {name}
       </h3>
       <h4 className="font-heebo text-base font-medium text-custom-primary">
-        It is a long established fact that a reader
+        {description}
       </h4>
 
-      <Button>Ver mais</Button>
+      <Link href={`${redirect}`}>
+        <Button>Ver mais</Button>
+      </Link>
     </div>
   );
 }
