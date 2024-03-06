19b4753fae501137e3b7f972f2f3cca0c09fe5cc
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2024-03-06 18:10:11 -0300

    :boom: fix: remove force cache from requests

--- lib/api.ts
+++ lib/api.ts
@@ -3,11 +3,9 @@ import * as queries from "./sanityQueries";
 import { TypeActiveProject } from "@/@types/type-active-project";
 
 export async function getActiveProjects() {
-  const result = await client.fetch(
-    queries.queryProjects,
-    { cache: "force-cache" },
-    { next: { tags: ["blogPost"] } },
-  );
+  const result = await client.fetch(queries.queryProjects, {
+    next: { tags: ["blogPost"] },
+  });
   return result;
 }
 
@@ -15,7 +13,6 @@ export async function getActiveProjectsById(id: TypeActiveProject) {
   const params = { id };
 
   const response = await client.fetch(queries.queryProjectsById, params, {
-    cache: "force-cache",
     next: { tags: ["blogPost"] },
   });
 
